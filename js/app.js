924df91f3e2a450668fb72c14d13c48a12c473ee
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-08-17 12:46:09 -0500

    Consultando la API y mostrando resultados en el HTML

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function validarFormulario(e) {
         mostrarAlerta('Agrega un término de busqueda');
         return;
     }
+
+    buscarImagenes(terminoBusqueda);
 }
 
 function mostrarAlerta(msj) {
@@ -39,4 +41,48 @@ function mostrarAlerta(msj) {
         }, 3000);
 
     }
+}
+
+function buscarImagenes(termino) {
+    const key = '22967234-ada95738245f23425f4cfe247';
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
+
+    fetch(url)
+        .then( rpta => rpta.json() )
+        .then ( result => {
+            mostrarImagenes(result.hits);
+        })
+}
+
+function mostrarImagenes(imagenes) {
+    console.log(imagenes);
+
+    while(resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    //Iterar sobre el arreglo de imagenes y construir el HTML
+    imagenes.forEach( img => {
+        const { previewURL, likes, views, largeImageURL } = img;
+
+        resultado.innerHTML += `
+            <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+                <div class="bg-white">
+                    <img class="w-full" src="${previewURL}" >
+                    <div class="p-4">
+                        <p class="font-bold"> ${likes} <span class="font-light">Me gusta </span> </p>
+                        <p class="font-bold"> ${views} <span class="font-light">Veces vista </span> </p>
+                    </div>
+
+                    <div class="p-3">
+                        <a  class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded p-1"
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer">
+                            Ver imagen
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        `;
+    })
 }
